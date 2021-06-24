e205d163a2426e83dff1c465b8bae8db9e47715d
Josh <xasummersfallx@msn.com>
2021-06-24 11:36:56 -0400

    Added all sections to Thymeleafed HomePage.

--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -16,8 +16,11 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Category horrorCategory = new Category("Horror", "Scary");
-        Category actionCategory = new Category("Action", "Michael Bay");
         Category comedyCategory = new Category("Comedy", "Funny Ha-ha");
+        Category fantasyCategory = new Category("Fantasy", "Dragons!");
+        Category actionCategory = new Category("Action", "Explosions!");
+        Category dramaCategory = new Category("Drama", "Right in the feels.");
+        Category sliceCategory = new Category("Slice of Life", "Wholesome, warm and fuzzy.");
         Anime sampleAnime = new Anime("Another", "P.A. Works Studio",
                 "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx11111-Y4QgkX8gJQCa.png",
                 "High school students suffer from a terrible, class-wide curse. The new kid in class finds out the hard way.",
@@ -25,6 +28,9 @@ public class Populator implements CommandLineRunner {
         categoryStorage.saveCategory(horrorCategory);
         categoryStorage.saveCategory(actionCategory);
         categoryStorage.saveCategory(comedyCategory);
+        categoryStorage.saveCategory(fantasyCategory);
+        categoryStorage.saveCategory(dramaCategory);
+        categoryStorage.saveCategory(sliceCategory);
         animeStorage.saveAnime(sampleAnime);
     }
 }
